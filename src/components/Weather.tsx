12b84ee13750ba16d43a3c8355361cf589e66cf2
Wonseok <dnjstjr970302@gmail.com>
2023-10-04 04:20:36 +0900

    "새로운 img 추가, temperatureClass 조건문 수정"

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -29,15 +29,19 @@ export default function Weather() {
     const temperature = weatherData.main.temp;
     if (temperature >= 23 && temperature <= 28) {
       temperatureClass = 'hot-background';
-    } else if (temperature >= 12 && temperature <= 22) {
+    } else if (temperature >= 17 && temperature <= 22) {
       temperatureClass = 'warm-background';
+    } else if (temperature >= 12 && temperature <= 16) {
+      temperatureClass = 'fall-background';
     } else {
       temperatureClass = 'cold-background';
     }
 
-    // 비올 확률에 따라 배경 이미지 설정
+    // 비 또는 눈 올 확률에 따라 배경 이미지 설정
     if (weatherData.rain && weatherData.rain['1h'] > 0) {
       temperatureClass = 'rainy-background';
+    } else if (weatherData.snow && weatherData.snow['1h'] > 0) {
+      temperatureClass = 'snow-background';
     }
   }
 
@@ -59,13 +63,15 @@ export default function Weather() {
       <Navbar />
       {weatherData ? (
         <div className={`weather-info ${temperatureClass}`}>
-          <h2>{transformCityName(weatherData.name)}</h2>
-          <p>날씨: {weatherData.weather[0].description}</p>
-          <p>온도: {weatherData.main.temp}°C</p>
-          <p>습도: {weatherData.main.humidity}%</p>
-          <p>기압: {weatherData.main.pressure} hPa</p>
-          {weatherData.rain && <p>비 예측: {weatherData.rain['1h']}mm</p>}
-          {weatherData.snow && <p>눈 예측: {weatherData.snow['1h']}mm</p>}
+          <div style={{ backgroundColor: 'white' }}>
+            <h2>{transformCityName(weatherData.name)}</h2>
+            <p>날씨: {weatherData.weather[0].description}</p>
+            <p>온도: {weatherData.main.temp}°C</p>
+            <p>습도: {weatherData.main.humidity}%</p>
+            <p>기압: {weatherData.main.pressure} hPa</p>
+            {weatherData.rain && <p>비 예측: {weatherData.rain['1h']}mm</p>}
+            {weatherData.snow && <p>눈 예측: {weatherData.snow['1h']}mm</p>}
+          </div>
         </div>
       ) : (
         <p>날씨 정보를 불러오는 중입니다...</p>
