334b7bc7342a49266a5ca17a73c9e11da2ecd6b9
Wonseok <dnjstjr970302@gmail.com>
2023-10-30 02:52:48 +0900

    Styles/signup 페이지 스타일

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -2,8 +2,13 @@ import React, { useState, ChangeEvent, FormEvent, useEffect } from 'react';
 import { auth, createUser, signIn, signInWithGoogle } from '../api/firebase';
 import { useNavigate } from 'react-router-dom';
 import { User, onAuthStateChanged } from 'firebase/auth';
-import { Container, FormContainer, LogoContainer } from './SignUpStyles';
-import logo from '/asset/Logo.png';
+import {
+  ModalContainer,
+  ModalContent,
+  Container,
+  FormContainer,
+  Title,
+} from './SignUpStyles';
 
 export default function SignUp() {
   const [email, setEmail] = useState('');
@@ -56,42 +61,45 @@ export default function SignUp() {
     setIsLogin(!isLogin);
   };
 
-  const buttonText = isLogin ? 'Login' : 'Sign Up';
+  const buttonText = isLogin ? '로그인' : '회원가입';
 
   return (
-    <Container>
-      <LogoContainer />
-      <div>{buttonText}</div>
-      {user ? (
-        <div>
-          <p>이미 로그인되어 있습니다.</p>
-          <button onClick={() => navigate('/clothesinfo')}>
-            Clothes Info 페이지로 이동
-          </button>
-        </div>
-      ) : (
-        <FormContainer onSubmit={handleAuth}>
-          <input
-            type='email'
-            placeholder='Email'
-            value={email}
-            onChange={handleEmailChange}
-          />
-          <input
-            type='password'
-            placeholder='Password'
-            value={password}
-            onChange={handlePasswordChange}
-          />
-          <button type='submit'>{buttonText}</button>
-          <button onClick={signInWithGoogle}>Google로 로그인</button>
-          <p onClick={toggleAuthMode} style={{ cursor: 'pointer' }}>
-            {isLogin
-              ? '회원이 아니신가요? 회원가입'
-              : '이미 회원이신가요? 로그인'}
-          </p>
-        </FormContainer>
-      )}
-    </Container>
+    <ModalContainer>
+      <ModalContent>
+        <Container>
+          <Title>{buttonText}</Title>
+          {user ? (
+            <div>
+              <p>이미 로그인되어 있습니다.</p>
+              <button onClick={() => navigate('/clothesinfo')}>
+                Clothes Info 페이지로 이동
+              </button>
+            </div>
+          ) : (
+            <FormContainer onSubmit={handleAuth}>
+              <input
+                type='email'
+                placeholder='Email'
+                value={email}
+                onChange={handleEmailChange}
+              />
+              <input
+                type='password'
+                placeholder='Password'
+                value={password}
+                onChange={handlePasswordChange}
+              />
+              <button type='submit'>{buttonText}</button>
+              <button onClick={signInWithGoogle}>구글 아이디로 로그인</button>
+              <p onClick={toggleAuthMode} style={{ cursor: 'pointer' }}>
+                {isLogin
+                  ? '회원이 아니신가요? 회원가입'
+                  : '이미 회원이신가요? 로그인'}
+              </p>
+            </FormContainer>
+          )}
+        </Container>
+      </ModalContent>
+    </ModalContainer>
   );
 }
--- src/pages/SignUpStyles.ts
+++ src/pages/SignUpStyles.ts
@@ -1,19 +1,67 @@
 import styled from 'styled-components';
 
+export const ModalContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 1000;
+`;
+
+export const ModalContent = styled.div`
+  width: 600px;
+  height: 900px;
+  background-color: white;
+  border-radius: 12px;
+  padding: 20px;
+`;
+
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100vh;
-  /* background-color: black;
-  color: white; */
+  margin-top: 70px;
 `;
 
 export const FormContainer = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  input {
+    width: 500px;
+    height: 80px;
+    border-radius: 12px;
+    border: 1px solid #8f8f8f;
+    background-color: #f9f9f9;
+  }
+  input + input {
+    margin-top: 18px;
+  }
+
+  input:nth-child(2) + button {
+    margin-top: 154px;
+  }
+
+  button {
+    width: 500px;
+    height: 80px;
+    border-radius: 12px;
+    background-color: #4a90e2;
+    border: none;
+    font-size: 18px;
+    color: white;
+  }
+
+  button + button {
+    margin-top: 18px;
+  }
 `;
 
 export const LogoContainer = styled.div`
@@ -23,4 +71,9 @@ export const LogoContainer = styled.div`
   background-repeat: no-repeat;
   background-size: contain;
   margin-bottom: 20px;
+`;
+
+export const Title = styled.div`
+  margin-bottom: 109px;
+  font-size: 40px;
 `;
