f95036c5b86b6ae4b68521fd51ac034c24a07066
Wonseok <dnjstjr970302@gmail.com>
2023-11-20 05:30:46 +0900

    feat/getOutfitByCategory함수 생성, Deatail페이지 데이터 가져오기

--- src/pages/ClothesDetail.tsx
+++ src/pages/ClothesDetail.tsx
@@ -1,5 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchClothesData } from '../api/firebase';
+import { getOutfitByCategory } from '../components/WeatherUtils';
 
-export default function ClothesDetail() {
-  return <div>옷 정보</div>;
+interface ClothesItem {
+  id: string;
+  name: string;
+  category: string;
+  imageURL: string;
+  description: string;
 }
+
+const ClothesDetail: React.FC = () => {
+  const [selectedCategory, setSelectedCategory] = useState<string>('전체');
+  const [clothesData, setClothesData] = useState<ClothesItem[]>([]);
+
+  const handleCategoryClick = (category: string) => {
+    setSelectedCategory(category);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const category = getOutfitByCategory(selectedCategory);
+        console.log('Selected Category:', category);
+        const data = await fetchClothesData(category);
+        console.log('Fetched Data:', data);
+        setClothesData(data);
+      } catch (error) {
+        console.error('데이터 가져오기 중 에러 발생:', error);
+      }
+    };
+
+    fetchData();
+  }, [selectedCategory]);
+
+  useEffect(() => {
+    console.log('Updated Clothes Data:', clothesData);
+  }, [clothesData]);
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => handleCategoryClick('전체')}>전체</button>
+        <button onClick={() => handleCategoryClick('아우터')}>아우터</button>
+        <button onClick={() => handleCategoryClick('상의')}>상의</button>
+        <button onClick={() => handleCategoryClick('하의')}>하의</button>
+      </div>
+      <div>
+        {clothesData.map((item, index) => (
+          <div key={index}>
+            <img
+              src={item.imageURL}
+              alt={item.name}
+              style={{ height: '100%', width: '100%', objectFit: 'cover' }}
+            />
+            <h3>{item.name}</h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ClothesDetail;
--- src/pages/LeisureDetail.tsx
+++ src/pages/LeisureDetail.tsx
@@ -1,5 +1,87 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchLeisureData } from '../api/firebase';
 
-export default function LeisureDetail() {
-  return <div>레저 정보입니다.</div>;
+interface LeisureItem {
+  id: string;
+  name: string;
+  category: string;
+  imageURL: string;
+  description: string;
 }
+
+const LeisureDetail: React.FC = () => {
+  const [selectedCategory, setSelectedCategory] = useState<string>('전체');
+  const [leisureData, setLeisureData] = useState<LeisureItem[]>([]);
+
+  const handleCategoryClick = (category: string) => {
+    setSelectedCategory(category);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let data: LeisureItem[] = [];
+
+        if (selectedCategory === '전체') {
+          const allData = await fetchLeisureData([
+            'spring',
+            'summer',
+            'fall',
+            'winter',
+          ]);
+          data = allData.flat();
+        } else {
+          const translatedCategory = translateCategory(selectedCategory);
+          data = await fetchLeisureData([translatedCategory]);
+        }
+
+        setLeisureData(data);
+      } catch (error) {
+        console.error('데이터 가져오기 중 에러 발생:', error);
+      }
+    };
+
+    fetchData();
+  }, [selectedCategory]);
+
+  const translateCategory = (category: string): string => {
+    switch (category) {
+      case '봄':
+        return 'spring';
+      case '여름':
+        return 'summer';
+      case '가을':
+        return 'fall';
+      case '겨울':
+        return 'winter';
+      default:
+        return category;
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => handleCategoryClick('전체')}>전체</button>
+        <button onClick={() => handleCategoryClick('봄')}>봄</button>
+        <button onClick={() => handleCategoryClick('여름')}>여름</button>
+        <button onClick={() => handleCategoryClick('가을')}>가을</button>
+        <button onClick={() => handleCategoryClick('겨울')}>겨울</button>
+      </div>
+      <div>
+        {leisureData.map((item, index) => (
+          <div key={index}>
+            <img
+              src={item.imageURL}
+              alt={item.name}
+              style={{ height: '100%', width: '100%', objectFit: 'cover' }}
+            />
+            <h3>{item.name}</h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default LeisureDetail;
