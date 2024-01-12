ca4553638189be387ed9d2241d05218d29e8bd6c
Wonseok <dnjstjr970302@gmail.com>
2024-01-12 22:34:58 +0900

    style/pretendard font

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,21 @@ import { BrowserRouter } from 'react-router-dom';
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
+
+@font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+  }
+
   * {
     box-sizing: border-box;
     
   }
 
   body {
-    font-family: Arial, sans-serif;
+    font-family: 'Pretendard-Regular', Arial, sans-serif;
   }
 
  
